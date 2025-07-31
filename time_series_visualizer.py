48e59d3c9b8d07b3ab89eb8c7e6143e325da1419
Tiisetso-Polori <118134452+Tiisetso-Polori@users.noreply.github.com>
2025-07-31 14:18:14 +0200

    done

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,18 +5,23 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv',index_col='date', parse_dates=True)
 
 # Clean data
-df = None
+df = df[
+    df['value'] >= df['value'].quantile(0.025) &
+    df['value'] <= df['value'].quantile(0.975)
+]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.plot(df.index, df['value'], color='red', linewidth=1.5
+                , label='Page Views')
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,12 +29,15 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy()
 
     # Draw bar plot
-
-
-
+    fig, ax = plt.subplots(figsize=(12, 6))
+    df_bar['year'] = df_bar.index.year
+    df_bar['month'] = df_bar.index.month_name()
+    ax.legend(title='Years')
+    sns.barplot(x='month', y='value', hue='year', data=df_bar,
+                palette='viridis', ax=ax)
 
 
     # Save image and return fig (don't change this part)
@@ -44,9 +52,14 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 6))
+    sns.boxplot(x='year', y='value', data=df_box, ax=ax1
+                , palette='viridis')
+    ax1.set_title('Year-wise Box Plot (Trend)')
 
-
-
+    sns.boxplot(x='month', y='value', data=df_box, ax=ax2
+                , palette='viridis')
+    ax2.set_title('Month-wise Box Plot (Seasonality)')
 
 
     # Save image and return fig (don't change this part)
